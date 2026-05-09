bb0ca907fa75721bd06a1fd0b99c224e4e72b5b2
DarkGuibrine <darkgui@protonmail.com>
2026-05-08 23:05:35 -0300

    update version

--- flake.nix
+++ flake.nix
@@ -5,29 +5,36 @@
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
   };
 
-  outputs = {
-    self,
-    nixpkgs,
-  }: let
-    systems = [
-      "x86_64-linux"
-      "aarch64-linux"
-      "x86_64-darwin"
-      "aarch64-darwin"
-    ];
-    forAllSystems = nixpkgs.lib.genAttrs systems;
-  in {
-    packages = forAllSystems (system: let
-      pkgs = import nixpkgs {
-        inherit system;
-        config.allowUnfree = true;
-      };
-    in {
-      default = pkgs.callPackage ./package.nix {};
-    });
+  outputs =
+    {
+      self,
+      nixpkgs,
+    }:
+    let
+      systems = [
+        "x86_64-linux"
+        "aarch64-linux"
+        "x86_64-darwin"
+        "aarch64-darwin"
+      ];
+      forAllSystems = nixpkgs.lib.genAttrs systems;
+    in
+    {
+      packages = forAllSystems (
+        system:
+        let
+          pkgs = import nixpkgs {
+            inherit system;
+            config.allowUnfree = true;
+          };
+        in
+        {
+          default = pkgs.callPackage ./package.nix { };
+        }
+      );
 
-    overlays.default = final: prev: {
-      hayase = self.packages.${final.system}.default;
+      overlays.default = final: prev: {
+        hayase = self.packages.${final.system}.default;
+      };
     };
-  };
 }
